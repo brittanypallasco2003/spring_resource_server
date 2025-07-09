692acb6b933de53981dec1d238685712363fd78f
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-09 13:40:05 -0500

    feat: configure access rules for public and private endpoints

--- src/main/java/com/brittany/spring_resource_server/config/SecurityConfig.java
+++ src/main/java/com/brittany/spring_resource_server/config/SecurityConfig.java
@@ -3,18 +3,24 @@ package com.brittany.spring_resource_server.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.brittany.spring_resource_server.service.JpaUserDetailsService;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -29,40 +35,54 @@ public class SecurityConfig {
     @Autowired
     private RsaKeyConfig rsaKeyConfig;
 
+    @Autowired
+    AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-        .csrf(csrf->csrf.disable())
-        .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
-        .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
-        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .httpBasic(Customizer.withDefaults())
-        .build();
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/auth/token/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/").permitAll().anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
 
     }
 
     @Bean
-    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password("{noop}1234").authorities("USER").build(),
-                User.withUsername("user2").password("{noop}1234").authorities("USER").build(),
-                User.withUsername("user3").password("{noop}1234").authorities("USER","ADMIN").build());
+    AuthenticationProvider authenticationProvider(JpaUserDetailsService jpaUserDetailsService,
+            PasswordEncoder passwordEncoder) throws Exception {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(jpaUserDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
     }
-    
+
     // Decodifica y valida tokens JWT entrantes (Usan la clave pública)
     @Bean
-    JwtDecoder jwtDecoder(){
+    JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(rsaKeyConfig.getPublicKey()).build();
     }
 
     // Genera y firma los tokens jwt salientes (Clave privada y pública)
     @Bean
     JwtEncoder jwtEncoder() {
-        JWK jwk= new RSAKey.Builder(rsaKeyConfig.getPublicKey()).privateKey(rsaKeyConfig.getPrivateKey()).build();
+        JWK jwk = new RSAKey.Builder(rsaKeyConfig.getPublicKey()).privateKey(rsaKeyConfig.getPrivateKey()).build();
         JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
 
         return new NimbusJwtEncoder(jwkSource);
     }
-
 
 }
