0eb19b6de5f9a0d4a59defc1f5e6ff4f6c1157e6
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-08 21:09:58 -0500

    chore: enable RsaKeyConfig with @EnableConfigurationProperties

--- src/main/java/com/brittany/spring_resource_server/SpringResourceServerApplication.java
+++ src/main/java/com/brittany/spring_resource_server/SpringResourceServerApplication.java
@@ -2,8 +2,12 @@ package com.brittany.spring_resource_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.brittany.spring_resource_server.config.RsaKeyConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyConfig.class)
 public class SpringResourceServerApplication {
 
 	public static void main(String[] args) {
