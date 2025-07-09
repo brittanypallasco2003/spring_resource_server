7cbd770091ce2414d82622a9ac6a6837e2aaf15b
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-08 21:06:40 -0500

    fix(config): use class instead of record for @ConfigurationPropertie

--- src/main/java/com/brittany/spring_resource_server/config/RsaKeyConfig.java
+++ src/main/java/com/brittany/spring_resource_server/config/RsaKeyConfig.java
@@ -4,8 +4,30 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "rsa")
-public record RsaKeyConfig(RSAPublicKey rsaPublicKey, RSAPrivateKey rsaPrivateKey) {
+public class RsaKeyConfig {
+
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;
+    
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+    public void setPublicKey(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+    public void setPrivateKey(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    
+
+
 
 }
