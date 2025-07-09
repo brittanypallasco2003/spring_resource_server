58c1ce0aaf70aae966d110d741dd2784e26b8220
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-09 13:34:25 -0500

    fix: use DTO instead of Authentication object in requests

--- src/main/java/com/brittany/spring_resource_server/controller/AuthController.java
+++ src/main/java/com/brittany/spring_resource_server/controller/AuthController.java
@@ -3,12 +3,13 @@ package com.brittany.spring_resource_server.controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brittany.spring_resource_server.controller.DTOs.TokenRequest;
 import com.brittany.spring_resource_server.service.TokenService;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -23,8 +24,8 @@ public class AuthController {
     }
 
     @PostMapping("/token")
-    public ResponseEntity<?> generateToken(Authentication authentication) {
-        String accessToken=tokenService.generateToken(authentication);
+    public ResponseEntity<?> generateToken(@RequestBody TokenRequest request) {
+        String accessToken=tokenService.generateToken(request);
 
         return ResponseEntity.ok().body(Map.of("accessToken",accessToken));
     }
