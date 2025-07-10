8e7844d55420132a4600ed6c9fbbfa2b908e9d5f
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-10 17:53:30 -0500

    feat: add JPA repository methods for RefreshToken entity

--- src/main/java/com/brittany/spring_resource_server/repositories/RefreshTokenRepository.java
+++ src/main/java/com/brittany/spring_resource_server/repositories/RefreshTokenRepository.java
@@ -3,11 +3,20 @@ package com.brittany.spring_resource_server.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.brittany.spring_resource_server.models.RefreshTokenModel;
+import com.brittany.spring_resource_server.models.UserModel;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshTokenModel, Long>{
+public interface RefreshTokenRepository extends JpaRepository<RefreshTokenModel, Long> {
     Optional<RefreshTokenModel> findByToken(String token);
-    void deleteByUsername(String username);
-
+    @Modifying
+    @Query(value = """
+                DELETE FROM refresh_tokens
+                WHERE user_id = (SELECT id FROM users WHERE username = :username)
+            """, nativeQuery = true)
+    void deleteByUsername(@Param("username") String username);
+    void deleteByUser(UserModel user);
 }
