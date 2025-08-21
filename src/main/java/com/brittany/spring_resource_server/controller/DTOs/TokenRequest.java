65aa5f83af54540a46ed9232310054c46c160b17
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-08-21 16:28:23 -0500

    feat: actualizar TokenRequest para usar GrantTypeEnum y agregar validaciones

--- src/main/java/com/brittany/spring_resource_server/controller/DTOs/TokenRequest.java
+++ src/main/java/com/brittany/spring_resource_server/controller/DTOs/TokenRequest.java
@@ -1,5 +1,14 @@
 package com.brittany.spring_resource_server.controller.DTOs;
 
-public record TokenRequest(String grantType, String username, String password, boolean withRefreshToken, String refreshToken) {
+import com.brittany.spring_resource_server.models.GrantTypeEnum;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record TokenRequest(
+
+        @NotNull(message = "El grantType es obligatorio") 
+        GrantTypeEnum grantType, String username, String password,
+        boolean withRefreshToken, String refreshToken) {
 
 }
