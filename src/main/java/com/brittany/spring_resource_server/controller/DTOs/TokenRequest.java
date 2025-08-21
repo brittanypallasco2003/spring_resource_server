f8d9d33bbc5f62823a79cce26f3484f9d283b19b
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-08-21 16:28:58 -0500

    feat: eliminar importación innecesaria de NotBlank en TokenRequest

--- src/main/java/com/brittany/spring_resource_server/controller/DTOs/TokenRequest.java
+++ src/main/java/com/brittany/spring_resource_server/controller/DTOs/TokenRequest.java
@@ -2,7 +2,6 @@ package com.brittany.spring_resource_server.controller.DTOs;
 
 import com.brittany.spring_resource_server.models.GrantTypeEnum;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record TokenRequest(
