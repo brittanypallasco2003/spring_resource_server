1c41a90f6a763ba16ba6076b79e5d52b40a9eae3
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-08-21 16:30:22 -0500

    refactor: separar responsabilidades en funciones en TokenServiceImpl para manejar tokens de acceso y refresh de manera más eficiente

--- src/main/java/com/brittany/spring_resource_server/service/TokenServiceImpl.java
+++ src/main/java/com/brittany/spring_resource_server/service/TokenServiceImpl.java
@@ -3,7 +3,12 @@ package com.brittany.spring_resource_server.service;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.stream.Collectors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,13 +17,13 @@ import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
-import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Service;
 
 import com.brittany.spring_resource_server.controller.Advice.EntityNotFoundException;
-import com.brittany.spring_resource_server.controller.Advice.ExpireTokenException;
+import com.brittany.spring_resource_server.controller.Advice.RefreshTokenException;
 import com.brittany.spring_resource_server.controller.DTOs.TokenRequest;
 import com.brittany.spring_resource_server.controller.DTOs.TokenResponse;
+import com.brittany.spring_resource_server.models.GrantTypeEnum;
 import com.brittany.spring_resource_server.models.RefreshTokenModel;
 import com.brittany.spring_resource_server.models.UserModel;
 import com.brittany.spring_resource_server.repositories.RefreshTokenRepository;
@@ -29,7 +34,6 @@ public class TokenServiceImpl implements TokenService {
 
   private final JwtEncoder jwtEncoder;
 
-  private final JwtDecoder jwtDecoder;
 
   private final AuthenticationManager authenticationManager;
 
@@ -42,7 +46,6 @@ public class TokenServiceImpl implements TokenService {
     this.jwtEncoder = jwtEncoder;
     this.authenticationManager = authenticationManager;
     this.refreshTokenRepository = refreshTokenRepository;
-    this.jwtDecoder = jwtDecoder;
     this.userRepository = userRepository;
   }
 
@@ -50,22 +53,49 @@ public class TokenServiceImpl implements TokenService {
   private String userGenerator;
 
   @Override
-  public TokenResponse generateToken(TokenRequest request) {
+  public TokenResponse generateToken(TokenRequest request) {;
+    List<String> dataToken=new ArrayList<>();
 
-    String subject = null;
-    String scope = null;
-    String refreshToken = null;
-    Instant now = Instant.now();
+    if (request.grantType()==GrantTypeEnum.password) {
+      dataToken=generateNewRefreshToken(request);
 
-    if (request.grantType().equals("password")) {
-      // 1. Autenticar usuario
+    } else if (request.grantType()==GrantTypeEnum.refreshToken) {
+     dataToken=getStoredRefreshToken(request);
+    }
+
+    String subject=dataToken.get(0);
+    String authorities=dataToken.get(1);
+    String refreshToken=dataToken.get(2);
+
+    Instant instant = Instant.now();
+
+    JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+        .subject(subject)
+        .issuedAt(instant)
+        .expiresAt(instant.plus(1, ChronoUnit.HOURS))
+        .issuer(userGenerator)
+        .claim("authorities", authorities)
+        .build();
+
+    String accessToken = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
+
+    return new TokenResponse(accessToken, refreshToken, "Bearer", 3600L);
+
+  }
+
+
+  private List<String> generateNewRefreshToken(TokenRequest request){
+     // 1. Autenticar usuario
+      if (request.username() ==null || request.password()==null) {
+        throw new IllegalArgumentException("Username y password obligatorios");
+      }
       UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
           request.username(), request.password());
 
       Authentication userAuthenticated = authenticationManager.authenticate(authenticationToken);
 
-      subject = userAuthenticated.getName();
-      scope = userAuthenticated.getAuthorities().stream().map(auth -> auth.getAuthority())
+      String subject = userAuthenticated.getName();
+      String scope = userAuthenticated.getAuthorities().stream().map(auth -> auth.getAuthority())
           .collect(Collectors.joining(" "));
 
       UserModel userDb = userRepository.findByUsername(subject)
@@ -75,46 +105,43 @@ public class TokenServiceImpl implements TokenService {
       refreshTokenRepository.deleteByUser(userDb);
 
       // Crear un nuevo refresh token
+      Instant now = Instant.now();
       RefreshTokenModel newToken = new RefreshTokenModel();
       newToken.setToken(UUID.randomUUID().toString());
       newToken.setExpiryDate(now.plus(7, ChronoUnit.DAYS));
 
       userDb.addTokenRefresh(newToken);
       UserModel userUpdated = userRepository.save(userDb);
-      refreshToken = newToken.getToken();
-    } else if (request.grantType().equals("refreshToken")) {
-      RefreshTokenModel storedToken = refreshTokenRepository.findByToken(request.refreshToken())
-          .orElseThrow(() -> new EntityNotFoundException("Refresh token inválido"));
+      String refreshToken = newToken.getToken();
 
+      return new ArrayList<>(Arrays.asList(subject, scope, refreshToken));
+
+  }
+
+  private List<String>getStoredRefreshToken(TokenRequest request){
+    if (request.refreshToken()==null) {
+        throw new IllegalArgumentException("Refresh token es obligatorio para grantType=refreshToken");
+      }
+      RefreshTokenModel storedToken = refreshTokenRepository.findByToken(request.refreshToken())
+          .orElseThrow(() -> new RefreshTokenException("Refresh token inválido"));
+
+      Instant now=Instant.now();
       if (storedToken.getExpiryDate().isBefore(now)) {
         refreshTokenRepository.delete(storedToken);
-        throw new ExpireTokenException("Refresh token expirado");
+        throw new RefreshTokenException("Refresh token expirado");
 
       }
 
       UserModel userDb = storedToken.getUser();
-      subject = userDb.getUsername();
-      scope = userDb.getRoles().stream()
+      String subject = userDb.getUsername();
+      String scope = userDb.getRoles().stream()
           .map(role -> role.getRoleName().name())
           .collect(Collectors.joining(" "));
 
-      refreshToken = storedToken.getToken();
-    }
-
-    Instant instant = Instant.now();
-
-    JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-        .subject(subject)
-        .issuedAt(instant)
-        .expiresAt(instant.plus(1, ChronoUnit.HOURS))
-        .issuer(userGenerator)
-        .claim("authorities", scope)
-        .build();
-
-    String accessToken = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
-
-    return new TokenResponse(accessToken, refreshToken, "Bearer", 3600L);
-
+      String refreshToken = storedToken.getToken();
+    return new ArrayList<>(Arrays.asList(subject,scope, refreshToken));
   }
-
 }
+
+
+
