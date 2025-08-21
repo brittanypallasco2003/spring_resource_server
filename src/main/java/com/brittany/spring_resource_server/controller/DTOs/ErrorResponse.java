7dccc0bc86e303870dfd7205a46112cf893f0efd
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-08-21 16:28:43 -0500

    feat: agregar campos de estado, mensaje y timestamp a ErrorResponse

--- src/main/java/com/brittany/spring_resource_server/controller/DTOs/ErrorResponse.java
+++ src/main/java/com/brittany/spring_resource_server/controller/DTOs/ErrorResponse.java
@@ -1,5 +1,7 @@
 package com.brittany.spring_resource_server.controller.DTOs;
 
-public record ErrorResponse() {
+import java.time.LocalDateTime;
+
+public record ErrorResponse(int status, String message, LocalDateTime timestamp) {
 
 }
