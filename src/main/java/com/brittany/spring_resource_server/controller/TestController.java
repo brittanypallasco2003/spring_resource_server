94bd495f27ee1d5e69f9b5a69f004c59a7f8257a
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-08-22 17:47:04 -0500

    feat: agregar endpoints públicos y de usuario en TestController

--- src/main/java/com/brittany/spring_resource_server/controller/TestController.java
+++ src/main/java/com/brittany/spring_resource_server/controller/TestController.java
@@ -6,9 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class TestController {
 
-    @GetMapping("/hello")
-    public String getMethodName() {
-        return "hola mundo";
+    @GetMapping("/public/hello")
+    public String publicHello() {
+        return "Hola! Soy público, no necesitas token 👋";
     }
-    
+
+    @GetMapping("/user/hello")
+    public String userHello() {
+        return "Hola USER 🙋, tienes acceso con ROLE_USER o ROLE_ADMIN";
+    }
+
+    @GetMapping("/admin/hello")
+    public String adminHello() {
+        return "Hola ADMIN 👑, solo accesible con ROLE_ADMIN";
+    }
+
+    @GetMapping("/secure/hello")
+    public String secureHello() {
+        return "Hola! Esta ruta es segura 🔒, necesitas estar autenticado con cualquier rol";
+    }
+
 }
